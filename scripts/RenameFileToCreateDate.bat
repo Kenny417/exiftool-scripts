10bc4a5d19c30d7dfbbfa98d756cc4577f2529b6
Kenny <kennyj2369@gmail.com>
2021-03-09 18:13:06 -0600

    Added script to change file names to the CreateDate in YYYY-MM-DD format

---
+++ scripts/RenameFileToCreateDate.bat
@@ -0,0 +1,6 @@
+exiftool "-FileName<CreateDate" -d "%%Y-%%m-%%d_%%H%%M%%S%%%%-c.%%%%ue" D*.NEF
+exiftool "-FileName<CreateDate" -d "%%Y-%%m-%%d_%%H%%M%%S%%%%-c.%%%%ue" D*.JPG
+exiftool "-FileName<CreateDate" -d "%%Y-%%m-%%d_%%H%%M%%S%%%%-c.%%%%ue" D*.MOV
+exiftool "-FileName<CreateDate" -d "%%Y-%%m-%%d_%%H%%M%%S%%%%-c.%%%%ue" D*.tif
+exiftool "-FileName<CreateDate" -d "%%Y-%%m-%%d_%%H%%M%%S%%%%-c.%%%%ue" D*.dng
+
